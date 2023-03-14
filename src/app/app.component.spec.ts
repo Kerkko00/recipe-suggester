a52fc38c4cccff389851880ead2ae200ccea757b
Kerkko <kerkko.kyyro@outlook.com>
2023-03-14 19:14:58 +0200

    Fixed tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'recipe-suggester'`, () => {
+  it(`should have as title 'Recipe suggester'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('recipe-suggester');
+    expect(app.title).toEqual('Recipe suggester');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('recipe-suggester app is running!');
+    expect(compiled.querySelector('.content h1')?.textContent).toContain('Recipe suggester');
   });
 });
