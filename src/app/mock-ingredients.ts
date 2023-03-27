256b6969930985a4c2b84dad9ec2f1cff0a0f4b6
Kerkko <kerkko.kyyro@outlook.com>
2023-03-27 20:17:27 +0300

    Added ingredient interface with mock ingredients

---
+++ src/app/mock-ingredients.ts
@@ -0,0 +1,7 @@
+import { Ingredient } from "./ingredient";
+
+export const INGREDIENTS: Ingredient[] = [
+  { id: 3, name: 'Onion' },
+  { id: 4, name: 'Potato' },
+  { id: 5, name: 'Chicken' },
+];
